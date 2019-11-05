cdb4879dff1907df58af4e20cd5e252fa408b14a
v1c7or20 <victorpa793@gmail.com>
2019-11-05 11:39:04 -0500

    done

--- main.cpp
+++ main.cpp
@@ -4,26 +4,28 @@
 using namespace std;
 
 int piezas[10000], querries[10000];
+int total=0, ques=0, caseNum = 1,temp;
 
 int main(){
-    int total=0, ques=0, caseNum = 1,temp;
     cin >> total >> ques;
-    while (total != 0 or ques != 0){
-        for (int t = 0; t< total; ++t) {
+    while (total != 0 and ques != 0){
+        for (int t = 0; t< total; ++t){
             cin>>temp;
             piezas[t]=temp;
-        }for (int t = 0; t< ques; ++t) {
+        }
+        for (int t = 0; t< ques; ++t){
             cin>>temp;
             querries[t]=temp;
         }
         sort(piezas,piezas+total);
         cout << "CASE# " << caseNum << ":" << endl;
+        caseNum++;
+        int control=0;
         for(int i=0;i<ques;i++){
-            int control=0;
-            for(int j=0;j<total;j++)
-            {
-                if(querries[i]==piezas[j]&&control!=1){
-                    cout << querries[i] << " found at " << i+1 << endl;
+            control=0;
+            for(int j=0;j<total;j++){
+                if(querries[i]==piezas[j]){
+                    cout << querries[i] << " found at " << j+1 << endl;
                     control++;
                     break;
                 }
@@ -31,6 +33,7 @@ int main(){
             if (control!=1)
             cout << querries[i] << " not found" << endl;
         }
+        cin >> total >> ques;
     }
 
     return 0;
